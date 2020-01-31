6fb9196f95032e58ee3c6b51b7ea72f529ce3447
Luis Hilario <luishilariomtz@gmail.com>
2020-01-31 00:59:46 -0600

    Update secret

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,11 +2,11 @@ class JsonWebToken
   class << self
     def encode(payload, exp = 24.hours.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, 'qwertyuiop')
+      JWT.encode(payload, 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE1ODA1NDAzNTF9.HJslKmG1-8P9jHZIYMHM-nIpUs9pNrETRz5rEG-1B2g')
     end
     
     def decode(token)
-      body = JWT.decode(token, 'qwertyuiop')[0]
+      body = JWT.decode(token, 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE1ODA1NDAzNTF9.HJslKmG1-8P9jHZIYMHM-nIpUs9pNrETRz5rEG-1B2g')[0]
       HashWithIndifferentAccess.new body
     rescue
       nil
